2caf0b25043adae9c072fef82ded96773d9ebbf1
Ciprian <48862508+Sufferal@users.noreply.github.com>
2023-10-05 11:42:59 +0300

    fix: refactor as dependency injection (#4)

--- src/Notification/SecurityNotification.java
+++ src/Notification/SecurityNotification.java
@@ -9,9 +9,13 @@ public class SecurityNotification implements Notification {
   private List<Customer> customers;
   private String message;
 
+  public SecurityNotification(String message, List<Customer> customers) {
+    this.message = message;
+    this.customers = customers;
+  }
+
   public SecurityNotification(String message) {
     this.message = message;
-    this.customers = CustomerManager.getInstance().getAllCustomers();
   }
 
   @Override
